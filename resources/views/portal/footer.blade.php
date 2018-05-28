028949791061aab1c1bc2be57a0bb41133467eb2
cw <1026039774@qq.com>
2018-05-28 11:08:01 +0800

    sdf

--- resources/views/portal/footer.blade.php
+++ resources/views/portal/footer.blade.php
@@ -6,7 +6,7 @@
                     <div class="copyright text-center">
                         <p>抵制不良游戏，拒绝盗版游戏。 注意自我保护，谨防受骗上当。 适度游戏益脑，沉迷游戏伤身。 合理安排时间，享受健康生活。</p>
                         <p>
-                            Copyright © 广州快鱼科技有限公司&nbsp;&nbsp;广州市天河区中西大道1132号汇鑫大厦五楼518&nbsp;&nbsp;020-38939825&nbsp;&nbsp;ceo@qt135.com&nbsp;&nbsp;
+                            Copyright © 广州快鱼文化传媒有限公司&nbsp;&nbsp;广州市天河区中西大道1132号汇鑫大厦五楼518&nbsp;&nbsp;020-38939825&nbsp;&nbsp;ceo@qt135.com&nbsp;&nbsp;
                             <a href="http://www.miitbeian.gov.cn/" target="_blank">备案号：粤ICP备18024803号-4</a>
                         </p>
                     </div>
